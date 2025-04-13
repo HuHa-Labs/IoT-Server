2848f8d0d46e53d124fd8f013af92b529f6b2667
phuongdoan13 <mphuongdoant@gmail.com>
2025-04-13 17:48:33 +1000

    update: inject KafkaService and reorganise

--- IoT-Server/Program.cs
+++ IoT-Server/Program.cs
@@ -1,5 +1,12 @@
+using IoT_Server.KafkaBackgroundService;
+
 var builder = WebApplication.CreateBuilder(args);
 
+#region Logging Configuration
+builder.Logging.SetMinimumLevel(LogLevel.Information);
+#endregion
+
+#region Dependency Injection
 builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true; // returns 406 for not supported file response request
@@ -13,7 +20,12 @@ builder.Services.AddProblemDetails(options =>
         ctx.ProblemDetails.Extensions.Add("Server Name", Environment.MachineName);
     };
 });
+
+builder.Services.AddHostedService<KafkaService>();
 builder.Services.AddSwaggerGen();
+#endregion
+
+#region Middleware Configuration
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -30,5 +42,6 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllers();
 });
 #pragma warning restore ASP0014
+#endregion
 
 app.Run();
