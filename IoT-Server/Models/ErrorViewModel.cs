ce22a3ff32ff31f0ee6c665f14132d766e3ba345
phuongdoan13 <mphuongdoant@gmail.com>
2025-04-13 14:28:25 +1000

    remove: remove example controller, then set up swagger

--- IoT-Server/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace IoT_Server.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- IoT-Server/obj/Debug/net8.0/IoT-Server.AssemblyInfo.cs
+++ IoT-Server/obj/Debug/net8.0/IoT-Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IoT-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f9323e3a9d6369ce1dab59ad9c2c07bc6f8fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IoT-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IoT-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
