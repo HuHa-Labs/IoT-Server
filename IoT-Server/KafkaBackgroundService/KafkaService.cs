ee4c8e25039c688336bc14323e7fa76113a10644
phuongdoan13 <mphuongdoant@gmail.com>
2025-05-07 22:47:58 +1000

    update: refactor service

--- IoT-Server/KafkaBackgroundService/KafkaService.cs
+++ IoT-Server/KafkaBackgroundService/KafkaService.cs
@@ -3,7 +3,7 @@ namespace IoT_Server.KafkaBackgroundService;
 public class KafkaService : BackgroundService 
 {
     readonly ILogger<KafkaService> _logger;
-
+    
     public KafkaService(ILogger<KafkaService> logger)
     {
         _logger = logger;
@@ -12,15 +12,14 @@ public class KafkaService : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.LogInformation("Kafka Service is starting.");
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            await PollDataFromKafkaAsync();
-        }
+        var consumeTokenSource = new CancellationTokenSource();
+        IKafkaHandlerStrategy kafkaHandlerStrategy = new KafkaHandlerStrategy();
+        await PollDataFromKafkaAsync(stoppingToken, consumeTokenSource.Token, kafkaHandlerStrategy);
     }
 
-    async Task PollDataFromKafkaAsync()
+    async Task PollDataFromKafkaAsync(CancellationToken stoppingToken, CancellationToken consumeCancellationToken, IKafkaHandlerStrategy kafkaHandlerStrategy)
     {
         _logger.LogInformation("Polling data from Kafka.");
-        await Task.Delay(5000);
+        await KafkaConsumer.StartAsync(stoppingToken, consumeCancellationToken, kafkaHandlerStrategy);
     }
 }
